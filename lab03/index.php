2e3fded4e92d74a816fa4e3fed9ae3cc9e2d08c6
mahmoud elbasiony <122675815+mahmoud-elbasiony@users.noreply.github.com>
2023-03-15 17:08:47 +0200

    Update index.php

--- lab03/index.php
+++ lab03/index.php
@@ -3,4 +3,4 @@ session_start();
 require("vendor/autoload.php");
 $counter=new Counter();
 $counter->increment_update();
-echo "<h3>".$counter->get_count()."</h3>";
+echo "<h3>Number of visits: ".$counter->get_count()."</h3>";
